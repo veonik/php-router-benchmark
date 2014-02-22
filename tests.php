f3efd1cfdd2fb372c8548c4d7722a718a8b446b8
Tyler Sommer <sommertm@gmail.com>
2014-02-22 05:01:48 -0800

    Fixed code style

--- tests.php
+++ tests.php
@@ -7,24 +7,24 @@ use TylerSommer\Nice\Benchmark\Benchmark;
  */
 function setupFastRoute(Benchmark $benchmark, $routes, $args)
 {
-    $argString = implode('/', array_map(function($i) { return "{arg$i}"; }, range(1, $args)));
+    $argString = implode('/', array_map(function ($i) { return "{arg$i}"; }, range(1, $args)));
     $lastStr = '';
-    $router = FastRoute\simpleDispatcher(function($router) use($routes, $argString, &$lastStr) {
+    $router = FastRoute\simpleDispatcher(function ($router) use ($routes, $argString, &$lastStr) {
             for ($i = 0, $str = 'a'; $i < $routes; $i++, $str++) {
                 $router->addRoute('GET', '/' . $str . '/' . $argString, 'handler' . $i);
                 $lastStr = $str;
             }
         });
 
-//    $benchmark->register('FastRoute - first route', function() use($router, $argString) {
+//    $benchmark->register('FastRoute - first route', function () use ($router, $argString) {
 //            $route = $router->dispatch('GET', '/a/' . $argString);
 //        });
 
-    $benchmark->register(sprintf('FastRoute - last route (%s routes)', $routes), function() use($router, $lastStr, $argString) {
+    $benchmark->register(sprintf('FastRoute - last route (%s routes)', $routes), function () use ($router, $lastStr, $argString) {
             $route = $router->dispatch('GET', '/' . $lastStr . '/' . $argString);
         });
 
-    $benchmark->register(sprintf('FastRoute - unknown route (%s routes)', $routes), function() use($router) {
+    $benchmark->register(sprintf('FastRoute - unknown route (%s routes)', $routes), function () use ($router) {
             $route = $router->dispatch('GET', '/not-even-real');
         });
 }
@@ -34,7 +34,7 @@ function setupFastRoute(Benchmark $benchmark, $routes, $args)
  */
 function setupPux(Benchmark $benchmark, $routes, $args)
 {
-    $argString = implode('/', array_map(function($i) { return ':arg' . $i; }, range(1, $args)));
+    $argString = implode('/', array_map(function ($i) { return ':arg' . $i; }, range(1, $args)));
     $lastStr = '';
     $router = new Pux\Mux;
     for ($i = 0, $str = 'a'; $i < $routes; $i++, $str++) {
@@ -42,15 +42,15 @@ function setupPux(Benchmark $benchmark, $routes, $args)
         $lastStr = $str;
     }
 
-//    $benchmark->register('Pux PHP - first route', function() use($router, $argString) {
+//    $benchmark->register('Pux PHP - first route', function () use ($router, $argString) {
 //            $route = $router->match('/a/' . $argString);
 //        });
 
-    $benchmark->register(sprintf('Pux PHP - last route (%s routes)', $routes), function() use($router, $lastStr, $argString) {
+    $benchmark->register(sprintf('Pux PHP - last route (%s routes)', $routes), function () use ($router, $lastStr, $argString) {
             $route = $router->match('/' . $lastStr . '/' . $argString);
         });
 
-    $benchmark->register(sprintf('Pux PHP - unknown route (%s routes)', $routes), function() use($router) {
+    $benchmark->register(sprintf('Pux PHP - unknown route (%s routes)', $routes), function () use ($router) {
             $route = $router->match('GET', '/not-even-real');
         });
 }
@@ -60,7 +60,7 @@ function setupPux(Benchmark $benchmark, $routes, $args)
  */
 function setupSymfony2(Benchmark $benchmark, $routes, $args)
 {
-    $argString = implode('/', array_map(function($i) { return "{arg$i}"; }, range(1, $args)));
+    $argString = implode('/', array_map(function ($i) { return "{arg$i}"; }, range(1, $args)));
     $lastStr = '';
     $sfRoutes = new Symfony\Component\Routing\RouteCollection();
     $router = new Symfony\Component\Routing\Matcher\UrlMatcher($sfRoutes, new Symfony\Component\Routing\RequestContext());
@@ -68,16 +68,16 @@ function setupSymfony2(Benchmark $benchmark, $routes, $args)
         $sfRoutes->add($str, new Symfony\Component\Routing\Route('/' . $str . '/' . $argString, array('controller' => 'handler' . $i)));
         $lastStr = $str;
     }
-    
-//    $benchmark->register('Symfony 2 - first route', function() use($router, $argString) {
+
+//    $benchmark->register('Symfony 2 - first route', function () use ($router, $argString) {
 //            $route = $router->match('/a/' . $argString);
 //        });
 
-    $benchmark->register(sprintf('Symfony 2 - last route (%s routes)', $routes), function() use($router, $lastStr, $argString) {
+    $benchmark->register(sprintf('Symfony 2 - last route (%s routes)', $routes), function () use ($router, $lastStr, $argString) {
             $route = $router->match('/' . $lastStr . '/' . $argString);
         });
 
-    $benchmark->register(sprintf('Symfony 2 - unknown route (%s routes)', $routes), function() use($router) {
+    $benchmark->register(sprintf('Symfony 2 - unknown route (%s routes)', $routes), function () use ($router) {
             try {
                 $route = $router->match('/not-even-real');
             } catch (\Symfony\Component\Routing\Exception\ResourceNotFoundException $e) { }
