0ccd5cf08e19e6429c7b362da2fdc2591c6a1d3a
c9s <yoanlin93@gmail.com>
2015-09-12 13:46:24 +0800

    update benchmark result with php-r3

--- first-route-tests.php
+++ first-route-tests.php
@@ -27,6 +27,7 @@ function setupBenchmark($numIterations, $numRoutes, $numArgs)
 
     setupAura2($benchmark, $numRoutes, $numArgs);
     setupFastRoute($benchmark, $numRoutes, $numArgs);
+    setupR3($benchmark, $numRoutes, $numArgs);
     setupSymfony2($benchmark, $numRoutes, $numArgs);
     setupSymfony2Optimized($benchmark, $numRoutes, $numArgs);
     setupPux($benchmark, $numRoutes, $numArgs);
@@ -43,6 +44,36 @@ function getRandomParts()
         substr($rand, -10),
     );
 }
+
+
+/**
+ * Sets up R3 tests
+ */
+function setupR3(Benchmark $benchmark, $routes, $args)
+{
+    $argString = implode('/', array_map(function ($i) { return "{arg$i}"; }, range(1, $args)));
+    $str = $firstStr = $lastStr = '';
+    $router = r3_tree_create_persist("app", 10);
+    if (!r3_tree_is_compiled($router)) {
+        for ($i = 0; $i < $routes; $i++) {
+            list ($pre, $post) = getRandomParts();
+            $str = '/' . $pre . '/' . $argString . '/' . $post;
+            if (0 === $i) {
+                $firstStr = str_replace(array('{', '}'), '', $str);
+            }
+            $lastStr = str_replace(array('{', '}'), '', $str);
+            r3_tree_insert($router, $str, "handler" . $i);
+        }
+        r3_tree_compile($router);
+    }
+
+    $benchmark->register(sprintf('php-r3 - first route', $routes), function () use ($router, $firstStr) {
+            $data = r3_tree_match($router, $firstStr);
+        });
+}
+
+
+
 
 /**
  * Sets up FastRoute tests
